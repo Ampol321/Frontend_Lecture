e79356ba1228ddae5ec220c9447b7aa52dc1b5a0
Ampol321 <dellova36@gmail.com>
2023-09-07 09:08:29 +0800

    Frontend

---
+++ src/components/Create.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const Create = () => {
+
+    const [state, setState] = useState({
+        title: '',
+        content: '',
+        user: ''
+    });
+
+    const [title, content, user] = state;
+
+    const handleChange = name => event => {
+        console.log('name', name, 'event', event.target.value);
+        setState({ ...state, [name]: event.target.value });
+    }
+
+   
+
+    return (
+        <div>
+            <h1>Create Post</h1>
+            <br />
+            <form>
+                <div className="form-group">
+                    <label className='text-muted'>Title </label>
+                    <input type="text" className='form-control' placeholder='Post Title' required />
+                </div>
+                <div className='form-group'>
+                    <label className='text-muted'>Content </label>
+                    <textarea type="text" className='form-control' placeholder='Write Something...' required />
+                </div>
+                <div className="form-group">
+                    <label className='text-muted'>User </label>
+                    <input type="text" className='form-control' placeholder='Your Name' required />
+                </div>
+                <div>
+                    <button className='btn btn-primary' type='submit'>Create</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Create

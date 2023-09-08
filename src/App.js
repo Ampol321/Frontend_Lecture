3967d4f8780fa40d1e59c98f7bc5d56483ff6102
Ampol321 <dellova36@gmail.com>
2023-09-08 23:47:38 +0800

    Updated NavBar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,42 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Create from './components/Create';
 import Nav from './components/Nav';
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  const fetchPost = () => {
+    axios
+      .get(`http://127.0.0.1:8000/api/posts`)
+      // .get(`${process.env.REACT_APP_API}/posts`)
+      .then(response => {
+        console.log(response);
+        setPosts(response.data)
+      })
+      .catch(error => alert('Error fetching posts'));
+  }
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
   return (
-      <div className="App">
-        <Nav />
-      </div>
+    <div className="App ">
+      <Nav />
+      {posts.map((post, i) => (
+        <div className='container'>
+          <div className='row'>
+            <h2 className='col-4'>{post.title}</h2>
+            <p className='col-2'>{post.slug}</p>
+            <p className='col-2'>{post.content}</p>
+          </div>
+        </div>
+      ))};
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import RoutedApp from './components/RoutedApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <RoutedApp>
-    <App />
-  </RoutedApp>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <RoutedApp>
+      <App />
+    </RoutedApp>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

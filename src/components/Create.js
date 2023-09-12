dca1da6b9c0c07930ed0ea3379f4465df64a09c9
Ampol321 <dellova36@gmail.com>
2023-09-12 09:44:21 +0800

    Storing data

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import Title from './Title';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
 
@@ -9,29 +11,47 @@ const Create = () => {
         user: ''
     });
 
-    const {title, content, user} = state;
+    const navigate = useNavigate()
+
+    const { title, content, user } = state;
 
     const handleChange = name => event => {
         console.log('name', name, 'event', event.target.value);
         setState({ ...state, [name]: event.target.value });
     }
 
+    const handleSubmit = event => {
+        event.preventDefault();
+        // axios.post(`${process.env.REACT_APP_API}/posts`, { title, content, user })
+        axios.post(`http://127.0.0.1:8000/api/posts`, { title, content, user })
+            .then(response => {
+                console.log(response)
+                setState({ ...state, title: '', content: '', user: '' });
+                alert(`Post title ${response.data.title} is created`);
+                return navigate("/");
+            })
+            .catch(error => {
+                console.log(error.response)
+                alert(error.response.data.error);
+            })
+    };
+
     return (
         <div>
-            <Title name='Create a new post'/>
+            <Title name='Create a new post' />
             <br />
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label className='text-muted'>Title </label>
-                    <input type="text" className='form-control' placeholder='Post Title' required onChange={handleChange('title')} value={title}/>
+                    <input type="text" className='form-control' placeholder='Post Title' required onChange={handleChange('title')} value={title} />
                 </div>
                 <div className='form-group'>
                     <label className='text-muted'>Content </label>
-                    <textarea type="text" className='form-control' placeholder='Write Something...' required onChange={handleChange('content')} value={content}/>
+                    <textarea type="text" className='form-control' placeholder='Write Something...' required onChange={handleChange('content')} value={content} />
                 </div>
                 <div className="form-group">
                     <label className='text-muted'>User </label>
-                    <input type="text" className='form-control' placeholder='Your Name' required onChange={handleChange('user')} value={user}/>
+                    <input type="text" className='form-control' placeholder='Your Name' required onChange={handleChange('user')} value={user} />
                 </div>
                 <div>
                     <button className='btn btn-primary' type='submit'>Create</button>

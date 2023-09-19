7bc8fa91f4bd901872c2dd6f7aaab58a67689caf
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-09-19 15:22:12 +0800

    Update Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,17 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
     return (
         <div>
-            <nav className='navbar navbar-light bg-light'>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <Link className="navbar-brand" to="/"> <img src="/images/React.png" width="35" height="35" alt="" /></Link>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/create">Create</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            {/* <nav className='navbar navbar-light bg-light'>
                 <ul className='nav nav-tabs'>
                     <li className='nav-item pr-3 pt-3 pb-3'>
                         <Link to="/">Home</Link>
@@ -13,7 +23,7 @@ const Nav = () => {
                         <Link to="/create">Create</Link>
                     </li>
                 </ul>
-            </nav>
+            </nav> */}
         </div>
     )
 }

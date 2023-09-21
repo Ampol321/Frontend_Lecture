3aea2ebce832c49edc29c40a921375523920a617
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-09-21 09:54:05 +0800

    Login link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,14 +4,20 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <Link className="navbar-brand" to="/"> <img src="/images/React.png" width="35" height="35g  " alt="" /></Link>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/create">Create</Link>
                         </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="#">Login</Link>
+                        </li>
                     </ul>
+                    {/* <li className="nav-item">
+                        <Link className="nav-link" to="#">Login</Link>
+                    </li> */}
                 </div>
             </nav>
             {/* <nav className='navbar navbar-light bg-light'>

0dc2d1b7501cf85397d704811f6087eb39a2c727
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-09-21 10:32:52 +0800

    Login Form

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,58 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
 
 const Nav = () => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <Link className="navbar-brand" to="/"> <img src="/images/React.png" width="35" height="35g  " alt="" /></Link>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/create">Create</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="#">Login</Link>
-                        </li>
                     </ul>
-                    {/* <li className="nav-item">
-                        <Link className="nav-link" to="#">Login</Link>
-                    </li> */}
+                    <Button variant="outlined" onClick={handleClickOpen}>Login</Button>
+                        <Dialog open={open} onClose={handleClose}>
+                            <DialogTitle>Subscribe</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>
+                                    To subscribe to this website, please enter your email address here. We
+                                    will send updates occasionally.
+                                </DialogContentText>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    label="Email Address"
+                                    type="email"
+                                    fullWidth
+                                    variant="standard"
+                                />
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Cancel</Button>
+                                <Button onClick={handleClose}>Subscribe</Button>
+                            </DialogActions>
+                        </Dialog>
                 </div>
             </nav>
             {/* <nav className='navbar navbar-light bg-light'>

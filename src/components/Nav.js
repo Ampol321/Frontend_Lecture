409f07d266f3bec2d5bf43ec61f280dda52c5268
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-09-21 11:30:11 +0800

    Login Token

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import { useNavigate } from 'react-router-dom';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import axios from 'axios';
 
 
 
 const Nav = () => {
+    const [state, setState] = useState({
+        email: '',
+        password: '',
+    });
+
     const [open, setOpen] = React.useState(false);
+    const { email, password } = state;
+    const navigate = useNavigate()
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -20,10 +29,30 @@ const Nav = () => {
     const handleClose = () => {
         setOpen(false);
     };
+
+    const handleChange = name => event => {
+        // console.log('name', name, 'event', event.target.value);
+        setState({ ...state, [name]: event.target.value });
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        axios.post(`http://127.0.0.1:8000/api/login`, { email, password })
+            .then(response => {
+                console.log(response)
+                setOpen(false);
+                return navigate("/");
+            })
+            .catch(error => {
+                console.log(error.response)
+                alert(error.response.data.error);
+            })
+    };
+
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link className="navbar-brand" to="/"> <img src="/images/React.png" width="35" height="35g  " alt="" /></Link>
+                <Link className="navbar-brand" to="/"> <img src="/images/React1.png" width="42" height="35g  " alt="" /></Link>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
@@ -31,28 +60,38 @@ const Nav = () => {
                         </li>
                     </ul>
                     <Button variant="outlined" onClick={handleClickOpen}>Login</Button>
-                        <Dialog open={open} onClose={handleClose}>
-                            <DialogTitle>Subscribe</DialogTitle>
-                            <DialogContent>
-                                <DialogContentText>
-                                    To subscribe to this website, please enter your email address here. We
-                                    will send updates occasionally.
-                                </DialogContentText>
+                    <Dialog open={open} onClose={handleClose}>
+                        <DialogTitle>Login</DialogTitle>
+                        <DialogContent>
+                            {/* <form onSubmit={handleSubmit}> */}
                                 <TextField
                                     autoFocus
                                     margin="dense"
-                                    id="name"
+                                    id="email"
                                     label="Email Address"
                                     type="email"
                                     fullWidth
                                     variant="standard"
+                                    onChange={handleChange('email')}
+                                    value={email}
                                 />
-                            </DialogContent>
-                            <DialogActions>
-                                <Button onClick={handleClose}>Cancel</Button>
-                                <Button onClick={handleClose}>Subscribe</Button>
-                            </DialogActions>
-                        </Dialog>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="password"
+                                    label="Password"
+                                    type="password"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={handleChange('password')}
+                                    value={password}
+                                />
+                            {/* </form> */}
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleSubmit}>Login</Button>
+                        </DialogActions>
+                    </Dialog>
                 </div>
             </nav>
             {/* <nav className='navbar navbar-light bg-light'>
